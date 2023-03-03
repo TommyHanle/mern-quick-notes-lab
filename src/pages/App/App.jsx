59cca6165ba9835ed900df1f807c4b79a6a3ec47
TommyHanle <tomboehan@gmail.com>
2023-03-03 10:51:55 -0800

    Finish

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,12 +3,17 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar'
 
 export default function App() {
   const [ user, setUser ] = useState(getUser())
+  const [ notes, setNotes ] = useState([])
+  const [ newNote, setNewNote ] = useState('')
+
+  const handleAddNote = () => {
+    setNotes([...notes, {text: newNote, createdAt: new Date()}])
+    setNewNote('')
+  }
 
   return (
     <main className="App">
@@ -16,10 +21,17 @@ export default function App() {
         user ?
         <>
           <NavBar user={user} setUser={setUser} />
-          <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
+          <div>
+            <input type="text" value={newNote} onChange={(e) => setNewNote(e.target.value)} />
+            <button onClick={handleAddNote}>Add Note</button>
+          </div>
+          {notes.length === 0 && <p>No Notes Yet!</p>}
+          {notes.map((note, idx) => (
+            <div key={idx}>
+              <p>{note.text}</p>
+              <p>{note.createdAt.toLocaleString()}</p>
+            </div>
+          ))}
         </>
         :
         <AuthPage setUser={setUser} />
@@ -27,5 +39,3 @@ export default function App() {
     </main>
   );
 }
-
-
